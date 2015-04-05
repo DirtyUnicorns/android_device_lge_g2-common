844fc36719a02112bcd35608c3bf0fdb37946428
Brinly Taylor <uberlaggydarwin@gmail.com>
2015-04-05 10:16:14 +0930

    g2: Clear up inherrited e2fsck package
    This seems to be inherrited from
    https://github.com/CyanogenMod/android_vendor_cm/blob/4534c7ae3bddb435944be7c022ee0ca5e8b62f97/config/common.mk#L167
    
    Change-Id: Iedea2a7dbf641221a010412a71880314b883976c
    Signed-off-by: Zdrowy Gosciu <ZdrowyGosciu+GITHUB@gmail.com>

--- g2.mk
+++ g2.mk
@@ -130,10 +130,6 @@ PRODUCT_TAGS += dalvik.gc.type-precise
 PRODUCT_PACKAGES += \
     com.android.future.usb.accessory
 
-# Filesystem management tools
-PRODUCT_PACKAGES += \
-    e2fsck
-
 PRODUCT_PACKAGES += \
     libgenlock \
     liboverlay \
