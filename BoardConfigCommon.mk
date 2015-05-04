07b5152c223a2901a214181b578284f8e2814fa3
Greg T. Wallace <greg@gregtwallace.com>
2015-05-03 22:49:29 -0400

    Add TWRP files and settings.
    Conflicts:
    	BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -146,3 +146,48 @@ TARGET_SPECIFIC_HEADER_PATH := device/lge/g2-common/include
 
 #Fix reboot when the screen is locked
 BOARD_NO_WIFI_HAL := true
+
+
+###                                     ###
+### Specific Options for TWRP Building  ###
+###                                     ###
+
+# Common Options
+RECOVERY_VARIANT := twrp
+DEVICE_RESOLUTION := 1080x1920
+RECOVERY_SDCARD_ON_DATA := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+TW_BRIGHTNESS_PATH := "/sys/devices/mdp.0/qcom\x2cmdss_fb_primary.175/leds/lcd-backlight/brightness"
+TW_MAX_BRIGHTNESS := 255
+TW_NO_USB_STORAGE := true
+TW_INCLUDE_L_CRYPTO := true
+TARGET_RECOVERY_INITRC := device/lge/g2-common/twrp/init.rc
+
+# Post recovery boot script
+PRODUCT_COPY_FILES += device/lge/g2-common/twrp/postrecoveryboot.sh:recovery/root/sbin/postrecoveryboot.sh
+
+# Add touch to wake as its own init
+PRODUCT_COPY_FILES += device/lge/g2-common/twrp/init.recovery.touchtowake.rc:recovery/root/etc/init.recovery.touchtowake.rc
+
+# Enable f2fs filesystem tools
+TARGET_USERIMAGES_USE_F2FS := true
+
+##  Functions to properly set config and files based on existence of an External SD Card
+# Board has an External SD Card (i.e. f320 or lgl22)
+ifneq ($(filter f320 lgl22,$(TARGET_DEVICE)),)
+    BOARD_HAS_NO_REAL_SDCARD := false
+    TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+    TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+
+    PRODUCT_COPY_FILES += device/lge/g2-common/twrp/twrp.g2.has_sd.fstab:recovery/root/etc/twrp.fstab
+    PRODUCT_COPY_FILES += device/lge/g2-common/twrp/init.recovery.sd_card.rc:recovery/root/etc/init.recovery.sd_card.rc
+
+# OR
+# Board does not have an External SD Card
+else
+    BOARD_HAS_NO_REAL_SDCARD := true
+    PRODUCT_COPY_FILES += device/lge/g2-common/twrp/twrp.g2.no_sd.fstab:recovery/root/etc/twrp.fstab
+endif
+
---
+++ twrp/init.recovery.touchtowake.rc
@@ -0,0 +1,7 @@
+##
+## Adds touch to wake support
+##
+
+on fs
+    chown root shell /sys/devices/virtual/input/lge_touch/touch_gesture
+    write /sys/devices/virtual/input/lge_touch/touch_gesture 1
