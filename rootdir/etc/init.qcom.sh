553ff74fa0e8e4f53ac3c2d003bbe3b5bdfdd5ce
danielstuart14 <daniel.stuart14@gmail.com>
2015-08-22 19:03:22 -0400

    g2: init: Set correct cdma baseband ver.
    LS980 & VS980 use qcom-style baseband version.
    
    We also grep from modem.b18, since they don't have b21.
    
    I don't mind giving Daniel the credit, but the grep find is mine

--- rootdir/etc/init.qcom.sh
+++ rootdir/etc/init.qcom.sh
@@ -29,9 +29,16 @@
 target=`getprop ro.board.platform`
 platformid=`cat /sys/devices/system/soc/soc0/id`
 
+product=`getprop ro.build.product`
 # grep the modem partition for baseband version and set it
-setprop gsm.version.baseband `strings /firmware/image/modem.b21 | grep "^M8974A-" | head -1`
-
+case "$product" in
+        "ls980" | "vs980")
+        setprop gsm.version.baseband `strings /firmware/image/modem.b18 | grep -e "^MPSS.DI." | head -1`
+        ;;
+        "d800" | "d801" | "d802" | "d803" | "f320" | "l01f")
+        setprop gsm.version.baseband `strings /firmware/image/modem.b21 | grep "^M8974A-" | head -1`
+        ;;
+esac
 #
 # Function to start sensors for DSPS enabled platforms
 #
