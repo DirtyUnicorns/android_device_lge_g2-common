1e349383808ccac1f573dffe56d2a9bf51d379e6
Ethan Chen <intervigil@gmail.com>
2015-10-14 18:13:08 -0400

    g2-common: Reorganize init script makefile

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,25 +1,25 @@
 LOCAL_PATH := $(call my-dir)
 
-# Device init files
+# Init scripts
 ##  Functions to properly set config and files based on existence of an External SD Card
 # Board has an External SD Card (i.e. f320 or lgl22)
 ifneq ($(filter f320 lgl22,$(TARGET_DEVICE)),)
 include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.g2
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/alt/fstab.g2
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE            := fstab.g2
+LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/alt/fstab.g2
+LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 # OR
 # Board does not have an External SD Card
 else
 include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.g2
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/fstab.g2
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE            := fstab.g2
+LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/fstab.g2
+LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 endif
 
