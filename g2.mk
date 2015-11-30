0d0485357569dce9d6c56f02c7508fa00618fa1d
Ethan Chen <intervigil@gmail.com>
2015-11-29 20:57:11 -0600

    g2: Clean up media library listing
    * Remove libdivxdrmdecrypt (this is OSS)
    * Remove dashplayer
    * Remove libscale (unused)
    * Remove postproc (unused)
    * Remove QC HEVC decoder (fails to initialize)
    * Add missing libFileMux dependency (libwfdcommonutils is required)
    * Bring back SHIMDivxDrm (dependency of libdivxdrmdecrypt)
    * Bring back DivxDrm (dependency of SHIMDivxDrm)
    * Bring back mmosal (dependency of DivxDrm, Hevc, etc)
    
    Change-Id: I1964a34ef3bfff19157535067a42355ba0d3d14b

--- g2.mk
+++ g2.mk
@@ -148,7 +148,6 @@ PRODUCT_PACKAGES += \
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
-    libdashplayer \
     libdivxdrmdecrypt \
     libextmedia_jni \
     libOmxAacEnc \
@@ -159,12 +158,7 @@ PRODUCT_PACKAGES += \
     libOmxVdec \
     libOmxVenc \
     libOmxVidcCommon \
-    libqcmediaplayer \
-    libstagefrighthw \
-    qcmediaplayer
-
-PRODUCT_BOOT_JARS += \
-    qcmediaplayer
+    libstagefrighthw
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
