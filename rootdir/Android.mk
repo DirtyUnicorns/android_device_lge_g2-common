7ba5a3425b5d2cfd3d448649326a702c95d989b1
nobe1976 <charlesmc76@gmail.com>
2016-03-28 17:36:26 -0400

    More inline ramdisk scripts. This brings things more into there place for all devices.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -10,19 +10,7 @@ LOCAL_SRC_FILES         := etc/set_baseband.sh
 include $(BUILD_PREBUILT)
 
 # Init scripts
-##  Functions to properly set config and files based on existence of an External SD Card
-# Board has an External SD Card (i.e. f320 or lgl22)
-ifneq ($(filter f320 lgl22,$(TARGET_DEVICE)),)
-include $(CLEAR_VARS)
-LOCAL_MODULE            := fstab.g2
-LOCAL_MODULE_TAGS       := optional eng
-LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := etc/alt/fstab.g2
-LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-# OR
-# Board does not have an External SD Card
-else
+
 include $(CLEAR_VARS)
 LOCAL_MODULE            := fstab.g2
 LOCAL_MODULE_TAGS       := optional eng
@@ -30,18 +18,7 @@ LOCAL_MODULE_CLASS      := ETC
 LOCAL_SRC_FILES         := etc/fstab.g2
 LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
-endif
 
-# Init scripts
-ifneq ($(filter f320 lgl22,$(TARGET_DEVICE)),)
-include $(CLEAR_VARS)
-LOCAL_MODULE            := init.g2.rc
-LOCAL_MODULE_TAGS       := optional eng
-LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := etc/alt/init.g2.rc
-LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-else
 include $(CLEAR_VARS)
 LOCAL_MODULE            := init.g2.rc
 LOCAL_MODULE_TAGS       := optional eng
@@ -49,7 +26,6 @@ LOCAL_MODULE_CLASS      := ETC
 LOCAL_SRC_FILES         := etc/init.g2.rc
 LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
-endif
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := init.g2.power.rc
